e96768506876126f89c097212dac6ad1343b12a0
frank <franktian424@gmail.com>
2020-11-16 15:01:08 +0800

    add mujoco sim

--- robot_mujoco/laikago.py
+++ robot_mujoco/laikago.py
@@ -1,2 +1,82 @@
-from mujoco_py import load_model_from_path, MjSim, MjViewer
-import os
+# 这个文件是对laikago的机械特性和电气特性的仿真。
+
+import robot_mujoco.laikago_constant as laikago_constant
+from robot_bullet.laikago_constant import InitPose
+import mujoco_py as mj
+import math
+import numpy as np
+import random
+import time
+
+class Laikago(object):
+    def __init__(self,
+                 visual=False,
+                 time_step=laikago_constant.TIME_STEP,
+                 num_motors=laikago_constant.NUM_MOTORS,
+                 dofs_per_leg=laikago_constant.DOFS_PER_LEG,
+                 urdf_filename=laikago_constant.URDF_FILE,
+                 init_pose=InitPose.ON_ROCK,
+                 obs_delay=False,
+                 action_repeat=laikago_constant.ACTION_REPEAT,
+                 randomized=True,
+                 observation_noise_stdev=laikago_constant.SENSOR_NOISE_STDDEV,
+                 observation_history_len=laikago_constant.OBSERVATION_HISTORY_LEN,
+                 kp=laikago_constant.KP,
+                 kd=laikago_constant.KD,
+                 torque_limits=laikago_constant.TORQUE_LIMITS,
+                 mass_bound=laikago_constant.MASS_BOUND,
+                 inertia_bound=laikago_constant.INERTIA_BOUND,
+                 joint_f_bound=laikago_constant.JOINT_F_BOUND,
+                 toe_f_bound=laikago_constant.TOE_F_BOUND,
+                 g_bound=laikago_constant.G_BOUND,
+                 max_motor_angle_change_per_step=laikago_constant.MAX_MOTOR_ANGLE_CHANGE_PER_STEP):
+        self.visual = visual
+        self.model = mj.load_model_from_path(urdf_filename)
+        self.sim = mj.MjSim(self.model)
+        self.data = self.sim.data
+        self.time_step = time_step
+        self.num_motors = num_motors
+        self.num_legs = num_motors / dofs_per_leg
+        self._urdf_filename = urdf_filename
+        self._init_pose = init_pose
+        self._self_collision_enabled = self_collision_enabled
+        self.action_filter_enabled = action_filter_enabled
+        self.obs_delay = obs_delay
+        self._action_repeat = action_repeat
+        self.randomized = randomized
+        self._observation_noise_stdev = observation_noise_stdev
+        self.observation_history_len = observation_history_len
+        self._kp = kp
+        self._kd = kd
+        self._torque_limits = torque_limits
+        self.mass_bound = mass_bound
+        self.inertia_bound = inertia_bound
+        self.joint_f_bound = joint_f_bound
+        self.toe_f_bound = toe_f_bound
+        self.g_bound = g_bound
+        self.max_motor_angle_change_per_step = max_motor_angle_change_per_step
+
+        self.now_g = sum(g_bound) / 2
+        self.energy = 0
+        self._step_counter = 0
+        self._last_frame_time = 0
+        self._last_observation = np.zeros(34).tolist()
+
+        self._action_filter = self._build_action_filter()
+        self._last_action = None
+        self.reset(init_reset=True)
+        self.receive_observation()
+
+
+
+
+model = mj.load_model_from_path('./laikago_urdf/laikago.xml')
+sim = mj.MjSim(model)
+viewer = mj.MjViewer(sim)
+t = 0
+while True:
+    sim.data.ctrl[0] = math.cos(t / 10.) * 0.01
+    sim.data.ctrl[1] = math.sin(t / 10.) * 0.01
+    t += 1
+    sim.step()
+    viewer.render()
---
+++ robot_mujoco/laikago_constant.py
@@ -0,0 +1,97 @@
+import re
+import numpy as np
+import os
+ROBOT_DIR_NAME = os.path.dirname(__file__)
+URDF_DIR_NAME = os.path.join(ROBOT_DIR_NAME, 'laikago_urdf')
+URDF_FILE = os.path.join(URDF_DIR_NAME, 'laikago.xml')
+# URDF_DIR_NAME = os.path.join(ROBOT_DIR_NAME, 'laikago_model')
+# URDF_FILE = os.path.join(URDF_DIR_NAME, 'laikago_toes_limits.urdf')
+
+CAMERA_SETTING = {'camera_distance': 1.0,
+                  'camera_yaw': 0,
+                  'camera_pitch': -30,
+                  'render_width': 480,
+                  'render_height': 360}
+TIME_STEP = 0.001
+ACTION_REPEAT = 20
+NUM_MOTORS = 12
+DOFS_PER_LEG = 3
+
+ON_RACK_INIT_POSITION = [0, 0, 1]
+STAND_INIT_POSITION = [0, 0, 0.43]
+LIE_INIT_POSITION = [0, 0, 0.15]
+
+INIT_ORIENTATION = [0, 0, 0, 1]
+
+HIP_NAME_PATTERN = re.compile(r"\w{2}_hip_motor_2_chassis_joint")
+UPPER_NAME_PATTERN = re.compile(r"\w{2}_upper_leg_2_hip_motor_joint")
+LOWER_NAME_PATTERN = re.compile(r"\w{2}_lower_leg_2_upper_leg_joint")
+TOE_NAME_PATTERN = re.compile(r"jtoe\w{2}")
+
+MOTOR_NAMES = [
+    "FR_hip_motor_2_chassis_joint",
+    "FR_upper_leg_2_hip_motor_joint",
+    "FR_lower_leg_2_upper_leg_joint",
+    "FL_hip_motor_2_chassis_joint",
+    "FL_upper_leg_2_hip_motor_joint",
+    "FL_lower_leg_2_upper_leg_joint",
+    "RR_hip_motor_2_chassis_joint",
+    "RR_upper_leg_2_hip_motor_joint",
+    "RR_lower_leg_2_upper_leg_joint",
+    "RL_hip_motor_2_chassis_joint",
+    "RL_upper_leg_2_hip_motor_joint",
+    "RL_lower_leg_2_upper_leg_joint",
+]
+
+LIE_MOTOR_ANGLES = np.array([0, 90, -155,
+                   0, 90, -155,
+                   0, 180, -155,
+                   0, 180, -155]) * np.pi / 180
+
+STAND_MOTOR_ANGLES = np.array([-10, 30, -75,
+                   10, 30, -75,
+                   -10, 50, -75,
+                   10, 50, -75]) * np.pi / 180
+
+"""
+Randomization:随机化分为两部分，一部分是观测的随机，另一部分是动力学参数的随机。
+"""
+
+SENSOR_NOISE_STDDEV = {'IMU_angle': 0.001, 'IMU_rate': 0.01, 'motor_angle': 0.001, 'motor_velocity': 0.01}
+# related bound
+MASS_BOUND = [0.8, 1.2]
+INERTIA_BOUND = [0.5, 1.5]
+# abstract bound
+JOINT_F_BOUND = [0, 0.1]
+TOE_F_BOUND = [0.5, 1.25]
+G_BOUND = [9.7, 9.9]
+
+
+
+OBSERVATION_HISTORY_LEN = 100
+
+from enum import Enum
+
+
+class InitPose(Enum):
+    STAND = 1
+    LIE = 2
+    ON_ROCK = 3
+
+ABDUCTION_P_GAIN = 220.0
+ABDUCTION_D_GAIN = 0.3
+HIP_P_GAIN = 220.0
+HIP_D_GAIN = 2.0
+KNEE_P_GAIN = 220.0
+KNEE_D_GAIN = 2.0
+KP = [ABDUCTION_P_GAIN, HIP_P_GAIN, KNEE_P_GAIN,
+            ABDUCTION_P_GAIN, HIP_P_GAIN, KNEE_P_GAIN,
+            ABDUCTION_P_GAIN, HIP_P_GAIN, KNEE_P_GAIN,
+            ABDUCTION_P_GAIN, HIP_P_GAIN, KNEE_P_GAIN]
+KD = [ABDUCTION_D_GAIN, HIP_D_GAIN, KNEE_D_GAIN,
+            ABDUCTION_D_GAIN, HIP_D_GAIN, KNEE_D_GAIN,
+            ABDUCTION_D_GAIN, HIP_D_GAIN, KNEE_D_GAIN,
+            ABDUCTION_D_GAIN, HIP_D_GAIN, KNEE_D_GAIN]
+TORQUE_LIMITS = np.ones(12) * 40
+
+MAX_MOTOR_ANGLE_CHANGE_PER_STEP = 0.2
