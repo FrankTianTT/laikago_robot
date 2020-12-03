37253f69e15eff64ef90e58c43237df0ee8b396f
frank <franktian424@gmail.com>
2020-12-03 13:26:04 +0800

    imitation and stand-from-lie update

--- builder/tasks_bullet/standfromlie_task_bullet.py
+++ builder/tasks_bullet/standfromlie_task_bullet.py
@@ -19,18 +19,20 @@ class LaikagoStandFromLieBullet0(LaikagoStandFromLieBulletBase):
 
     @property
     def is_healthy(self):
-        return not (self.done_r_bullet(threshold=60) or
-                    self.done_p_bullet(threshold=60) or
-                    self.done_y_bullet(threshold=60) or
-                    self.done_height_bullet(threshold=0.15) or
-                    # self.done_region_bullet(threshold=1) or
-                    self.done_toe_contact())
+        return not (self.done_r_bullet(threshold=30) or
+                    self.done_p_bullet(threshold=30) or
+                    self.done_y_bullet(threshold=30) or
+                    self.done_height_bullet(threshold=0.25) or
+                    self.done_region_bullet(threshold=3) or
+                    self.done_toe_contact_long(threshold=30) or
+                    self.done_toe_distance(threshold=0.2))
 
     def cal_phi_function(self):
-        sum = self.reward_r_bullet(threshold=60) + self.reward_p_bullet(threshold=60) + \
-              self.reward_y_bullet(threshold=60) + self.reward_height_bullet(threshold=0.15) + \
-              self.reward_toe_contact_soft()
-        return sum * 2
+        sum = self.reward_r_bullet(threshold=30) + self.reward_p_bullet(threshold=30) + \
+              self.reward_y_bullet(threshold=30) + self.reward_height_bullet(threshold=0.25) + \
+              self.reward_toe_contact_soft() * 5 + self.reward_region_bullet(threshold=3) + \
+              self.reward_toe_distance(threshold=0.2)
+        return sum
 
     def update_reward(self):
         if self.is_healthy:
--- builder/tasks_bullet/standimitation_task_bullet.py
+++ builder/tasks_bullet/standimitation_task_bullet.py
@@ -12,12 +12,13 @@ KNEE_D_GAIN = 2.0
 
 class LaikagoStandImitationBulletBase(LaikagoTaskBullet):
 
-    def __init__(self, run_mode='train'):
-        super(LaikagoStandImitationBulletBase, self).__init__(run_mode,
-                                                              init_pose=InitPose.STAND)
-        # self.mode = 'no-die'
-        self.steps = 0
-        self.imit_action = np.array([-10, 30, -75,
+    def __init__(self,
+                 reward_mode='without_shaping',
+                 run_mode='train'):
+        super(LaikagoStandImitationBulletBase, self).__init__(run_mode=run_mode,
+                                                              reward_mode=reward_mode,
+                                                              init_pose=InitPose.LIE)
+        self.imitation_action = np.array([-10, 30, -75,
                                      10, 30, -75,
                                      -10, 50, -75,
                                      10, 50, -75]) * np.pi / 180
@@ -31,36 +32,30 @@ class LaikagoStandImitationBulletBase(LaikagoTaskBullet):
             ABDUCTION_D_GAIN, HIP_D_GAIN, KNEE_D_GAIN]
         self._torque_limits = np.ones(12) * 40
 
-    def reset(self, env):
-        self._env = env
-        self.steps = 0
-
-    def update(self):
-        self.steps += 1
-
-    def done(self):
-        if self.run_mode== 'no-die':
-            return False
-        if self.steps > 300:
-            return True
-        else:
-            return self.done_rp_bullet(threshold=30) + self.done_height_bullet(threshold=0.3)
-
-    def reward_imitation(self):
-        pos = np.array(self._env.get_history_angle()[0])
-        vel = np.array(self._env.get_history_velocity()[0])
-        target_pos = self.imit_action
-        target_vel = np.zeros(12)
-        motor_torques = -1 * (self._kp * (pos - target_pos)) - self._kd * (vel - target_vel)
-        reward = - np.sum(np.abs(motor_torques))
-        return 2/(1+math.exp(-reward/100))
-
 class LaikagoStandImitationBullet0(LaikagoStandImitationBulletBase):
 
-    def __init__(self, run_mode='train'):
-        super(LaikagoStandImitationBullet0, self).__init__(run_mode)
+    def __init__(self, run_mode='train', reward_mode='with_shaping',):
+        super(LaikagoStandImitationBullet0, self).__init__(run_mode=run_mode,
+                                                           reward_mode=reward_mode)
 
-    def reward(self):
-        self.add_reward(self.reward_imitation(), 1)
-        self.add_reward(self.reward_energy(), 1)
-        return self.get_sum_reward()
+    @property
+    def is_healthy(self):
+        return not (self.done_r_bullet(threshold=30) or
+                    self.done_p_bullet(threshold=30) or
+                    self.done_y_bullet(threshold=30) or
+                    self.done_height_bullet(threshold=0.25) or
+                    self.done_region_bullet(threshold=3) or
+                    self.done_toe_contact_long(threshold=30) or
+                    self.done_toe_distance(threshold=0.2))
+
+    def cal_phi_function(self):
+        pos = np.array(self._env.get_history_angle()[0])
+        vel = np.array(self._env.get_history_velocity()[0])
+        target_pos = self.imitation_action
+        target_vel = np.zeros(12)
+        motor_torques = -1 * (self._kp * (pos - target_pos)) - self._kd * (vel - target_vel)
+        return 10 / np.sum(np.abs(motor_torques))
+
+    def update_reward(self):
+        if self.is_healthy:
+            self.add_reward(1, 1)
