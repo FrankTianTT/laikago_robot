6fe4a7eb0c68075a715d97f262a7dceb6e8182af
frank <franktian424@qq.com>
2020-12-18 17:05:53 +0800

    update tasks

--- builder/tasks_mujoco/standup_task_mujoco.py
+++ builder/tasks_mujoco/standup_task_mujoco.py
@@ -21,6 +21,7 @@ class LaikagoStandUpMujocoBase(LaikagoTaskMujoco):
                     self.done_y_mujoco(threshold=30) or
                     self.done_height_mujoco(threshold=0.2))
 
+
 class LaikagoStandUpMujoco0(LaikagoStandUpMujocoBase):
 
     def __init__(self, run_mode='train', reward_mode='with_shaping'):
@@ -32,36 +33,9 @@ class LaikagoStandUpMujoco0(LaikagoStandUpMujocoBase):
         return not (self.done_r_mujoco(threshold=30) or
                     self.done_p_mujoco(threshold=30) or
                     self.done_y_mujoco(threshold=30) or
-                    self.done_height_mujoco(threshold=0.3) or
-                    self.done_region_mujoco(threshold=3) or
-                    self.done_toe_distance(threshold=0.1) or
-                    self.reward_toe_height_mujoco(threshold=0.05))
-
-    def cal_phi_function(self):
-        sum = self.reward_r_mujoco(threshold=30) + self.reward_p_mujoco(threshold=30) + \
-              self.reward_y_mujoco(threshold=30) + self.reward_height_mujoco(threshold=0.3) + \
-              self.reward_region_mujoco(threshold=3) + self.reward_toe_distance(threshold=0.1) + \
-              self.reward_toe_height_mujoco(threshold=0.05)
-        return sum
-
-    def update_reward(self):
-        if self.is_healthy:
-            self.add_reward(1, 1)
-
-class LaikagoStandUpMujoco1(LaikagoStandUpMujocoBase):
-
-    def __init__(self, run_mode='train', reward_mode='with_shaping'):
-        super(LaikagoStandUpMujoco1, self).__init__(run_mode=run_mode,
-                                                    reward_mode=reward_mode)
-
-    @property
-    def is_healthy(self):
-        return not (self.done_r_mujoco(threshold=30) or
-                    self.done_p_mujoco(threshold=30) or
-                    self.done_y_mujoco(threshold=30) or
                     self.done_height_mujoco(threshold=0.2) or
                     self.done_toe_distance(threshold=0.1) or
-                    self.reward_toe_contact_long(threshold=14))
+                    self.done_toe_contact_long(threshold=14))
 
     def cal_phi_function(self):
         sum = self.reward_r_mujoco(threshold=30) + self.reward_p_mujoco(threshold=30) + \
