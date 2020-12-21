1fb7e29247cfb73d2d7598861d8bd9745d8a00e7
franktian <franktian424@qq.com>
2020-12-21 16:19:18 +0800

    update tasks

--- builder/tasks_mujoco/standup_task_mujoco.py
+++ builder/tasks_mujoco/standup_task_mujoco.py
@@ -67,7 +67,34 @@ class LaikagoStandUpMujoco0_1(LaikagoStandUpMujocoBase):
               self.reward_y_mujoco(threshold=10) + self.reward_speed_mujoco(threshold=0.1) + \
               self.reward_height_mujoco(threshold=0.25) + \
               self.reward_toe_distance(threshold=0.1)
-        return sum / 5
+        return sum / 6
+
+    def update_reward(self):
+        if self.is_healthy:
+            self.add_reward(1, 1)
+
+class LaikagoStandUpMujoco0_2(LaikagoStandUpMujocoBase):
+
+    def __init__(self, run_mode='train', reward_mode='with_shaping'):
+        super(LaikagoStandUpMujoco0_2, self).__init__(run_mode=run_mode,
+                                                    reward_mode=reward_mode,
+                                                    contact_buffer_length=3)
+
+    @property
+    def is_healthy(self):
+        return not (self.done_r_mujoco(threshold=10) or
+                    self.done_p_mujoco(threshold=10) or
+                    self.done_y_mujoco(threshold=10) or
+                    self.done_speed_mujoco(threshold=0.3) or
+                    self.done_height_mujoco(threshold=0.25) or
+                    self.done_toe_distance(threshold=0.1))
+
+    def cal_phi_function(self):
+        sum = self.reward_r_mujoco(threshold=10) + self.reward_p_mujoco(threshold=10) + \
+              self.reward_y_mujoco(threshold=10) + self.reward_speed_mujoco(threshold=0.3) + \
+              self.reward_height_mujoco(threshold=0.25) + \
+              self.reward_toe_distance(threshold=0.1)
+        return sum / 6
 
     def update_reward(self):
         if self.is_healthy:
