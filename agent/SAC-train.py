5d516ca43a46f1682ad50873522faf3d0a695748
franktian <franktian424@qq.com>
2020-12-28 22:16:51 +0800

    fix a bug in SAC-train.py

--- agent/SAC-train.py
+++ agent/SAC-train.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=net_arch)
 
     if args.load_version is not None:
-        best_model_dir = './{}/{}/SAC-v{}/logs/best_model.zip'.format(simulator, task_name, version)
+        best_model_dir = './{}/{}/SAC-v{}/logs/best_model.zip'.format(simulator, task_name, args.load_version)
         model = SAC.load(best_model_dir, device=torch.device('cuda:0'))
         model.set_env(env)
         model.tensorboard_log = tensorboard_log
